82e8b114961aa70d63ea0b2c32a3b1775265eda0
tomas <thuecas.ext@acciona.com>
2026-09-12 09:40:13 +0200

    first commit

---
+++ src/main/kotlin/com/tomas/membership/MembershipApiApplication.kt
@@ -0,0 +1,9 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MembershipApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<MembershipApiApplication>(*args)
+}
